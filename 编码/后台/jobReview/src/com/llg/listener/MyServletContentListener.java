8f26effe6049d164dc7427be4b451c9872a9dace
luolonggui <18280230896@163.com>
2019-04-27 16:52:10 +0800

    将文件存放路径发到xml中设置

---
+++ 编码/后台/jobReview/src/com/llg/listener/MyServletContentListener.java
@@ -0,0 +1,39 @@
+package com.llg.listener;
+
+import java.util.List;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.llg.util.FileUtil;
+
+public class MyServletContentListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		//½âÎöpath.xml
+		SAXReader saxReader = new SAXReader();
+		try {
+			Document document = saxReader.read(MyServletContentListener.class.getClassLoader().getResourceAsStream("path.xml"));
+			Element root = document.getRootElement();
+			List<Element> elements = root.elements();
+			for (Element element : elements) {
+				if(element.getQualifiedName().equals("localJobPath")) FileUtil.LOCAL_JOB_PATH = element.getText();
+				if(element.getQualifiedName().equals("localTempPath")) FileUtil.LOCAL_TEMP_PATH = element.getText();
+				if(element.getQualifiedName().equals("virtualJobPath")) FileUtil.VIRTUAL_JOB_PATH = element.getText();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
